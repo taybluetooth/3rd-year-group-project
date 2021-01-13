ffa16761cd59d4af6aff36f1c105424e2cac4545
Callum Taylor <callumtaylorbusiness@gmail.com>
2021-01-13 10:55:20 +0000

    Added in read by id route and delete by id route to complete CRUD stack.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,16 +2,34 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
 module.exports = (app) => {
-  app.get(`/api/user`, async (req, res) => {
+  app.get('/api/user', async (req, res) => {
     let users = await User.find();
     return res.status(200).send(users);
   });
 
-  app.post(`/api/user`, async (req, res) => {
+  app.post('/api/user', async (req, res) => {
     let user = await User.create(req.body);
     return res.status(201).send({
       error: false,
       user,
+    });
+  });
+
+  app.put(`/api/user/:id`, async (req, res) => {
+    const {id} = req.params;
+    let user = await User.findByIdAndUpdate(id, req.body);
+    return res.status(202).send({
+      error: false,
+      product
+    });
+  });
+
+  app.delete(`/api/user/:id`, async (req, res) => {
+    const {id} = req.params;
+    let user = await User.findByIdAndDelete(id);
+    return res.status(202).send({
+      error: false,
+      product
     });
   });
 };
