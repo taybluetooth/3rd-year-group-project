23d6528333d5987c2efdce3b66bc4fd0a1c709f7
Callum Taylor <callumtaylorbusiness@gmail.com>
2021-01-14 09:57:03 +0000

    Created instagram style layout.

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 // TEMP SOLUTION!
-import image from "../../public/edinburgh.jpg";
+import image from "../../public/london.jpg";
+
+const element = <FontAwesomeIcon icon={faEllipsisH} />
 
 export default (props) => {
 
   return (
-    <div class="bg-white rounded shadow border p-6 w-64">
-      <img class="object-contain h-48 w-full" src={image}></img>
-      <p class="text-gray-700 text-sm">{props.location} </p>
-      <p class="text-gray-700 text-sm">{props.description}< /p>
+    <div class="rounded overflow-hidden border w-full lg:w-4/12 md:w-6/12 bg-white mx-3 md:mx-0 lg:mx-0">
+    <div class="w-full flex justify-between p-3">
+      <div class="flex">
+        <div class="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
+          <img src="https://avatars0.githubusercontent.com/u/38799309?v=4" alt="profilepic"></img>
+        </div>
+        <span class="pt-1 ml-2 font-bold text-sm">CallumTaylor</span>
+      </div>
+      <span class="px-2 hover:bg-gray-300 cursor-pointer rounded">{element}</span>
     </div>
+    <img class="w-full bg-cover" src={image}></img>
+    <div class="px-3 pb-2">
+      <div class="pt-2">
+        <i class="far fa-heart cursor-pointer"></i>
+        <span class="text-sm text-gray-400 font-medium">5 likes</span>
+      </div>
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Callum Taylor</span> {props.description}
+        </div>
+      </div>
+    </div>
+  </div>
   );
 };
