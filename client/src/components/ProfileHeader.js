36959656bc2fa2fa0ff1e33a7270e08170f25906
taybluetooth <callumtaylorbusiness@gmail.com>
2021-01-18 17:08:39 +0000

    Implemented basic profile page layout.

---
+++ client/src/components/ProfileHeader.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default () => {
+
+  return (
+    <div class="overflow-hidden border w-full lg:w-6/12 md:w-6/12 mx-3 md:mx-0 lg:mx-0">
+      <div class="w-full flex justify-between p-3">
+        <div class="flex">
+          <div class="rounded-full h-40 w-40 bg-gray-500 flex items-center justify-center overflow-hidden">
+            <img src="https://avatars0.githubusercontent.com/u/39353470?s=460&u=c82cc7e746e25bdab580cdb83ec41dbb938a7d71&v=4" alt="profilepic"></img>
+          </div>
+          <div class="pt-4 ml-10">
+            <span class="font-bold text-lg"> Username </span>
+            <div class="grid grid-flow-col auto-cols-max gap-4 text-sm">
+              <div>0 posts</div>
+              <div>0 followers</div>
+              <div>0 following</div>
+            </div>
+            <div class="pt-3 text-sm">
+              <span class="font-bold"> Firstname </span>
+              <br />
+              <span> A basic description about the profile here. </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+}
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import ProfileHeader from '../components/ProfileHeader'
 
 function Profile() {
 
   return (
     <div>
       <Navbar />
+      <div class="flex justify-center container mx-auto p-3">
+        <ProfileHeader />
+      </div>
+      <div class="flex justify-center container mx-auto p-3">
+        <span class="font-bold"> Posts Here </span>
+      </div>
     </div>
   )
 
