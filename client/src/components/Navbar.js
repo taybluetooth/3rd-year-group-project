34001f1bdad21b7a7f06fd4e804056470828374f
taybluetooth <callumtaylorbusiness@gmail.com>
2021-01-18 16:15:23 +0000

    Updated Navbar.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => {
   return (
-    <nav class="bg-green-500">
+    <nav class="bg-indigo-700">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
@@ -10,7 +10,7 @@ export default () => {
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
-              <a href="#" class="bg-green-400	 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+              <a href="#" class="bg-indigo-400	 text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
               <a href="#" class="text-gray-300 hover:bg-green-400	 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Discover</a>
               <a href="#" class="text-gray-300 hover:bg-green-400	 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Notifications</a>
               <a href="#" class="text-gray-300 hover:bg-green-400	 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Account</a>
