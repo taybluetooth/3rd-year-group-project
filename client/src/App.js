2815542f58b86bb903ee6fed3a7a47b82540160c
Callum Taylor <callumtaylorbusiness@gmail.com>
2021-01-19 10:26:29 +0000

    Made sign in page the default route

--- client/src/App.js
+++ client/src/App.js
@@ -35,13 +35,13 @@ function App() {
         <Route path="/upload_post">
           <UploadPost />
         </Route>
-        <Route path="/login">
+        <Route path="/">
           <SignIn isLogin />
         </Route>
         <Route path="/profiletest">
           <Profile />
         </Route>
-        <Route path="/">
+        <Route path="/posts">
           <Posts />
         </Route>
       </Switch>
