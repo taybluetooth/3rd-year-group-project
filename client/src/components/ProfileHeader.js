305604e1a68d0fbfc0281962f8755cb8674669d3
Callum Taylor <callumtaylorbusiness@gmail.com>
2021-01-24 15:49:05 +0000

    Added edit profile button, made profile header responsive.

--- client/src/components/ProfileHeader.js
+++ client/src/components/ProfileHeader.js
@@ -19,25 +19,31 @@ function ProfileHeader() {
   }, []);
 
   return (
-    <div className="text-white overflow-hidden w-full lg:w-6/12 md:w-6/12 mx-3 md:mx-0 lg:mx-0">
-      <div className="w-full flex justify-between p-3">
-        <div className="flex container divider">
-          <div className="md:w-40 flex items-center justify-center overflow-hidden">
+    <div className="md:w-8/12 mx-0 mx-3 text-white overflow-hidden">
+      <div className="md:w-full container p-3">
+        <div className="flex items-center justify-center container divider">
+          <div className="md:flex-shrink-0 flex items-center justify-center overflow-hidden">
             <img
+              className="rounded-full w-40"
               src="https://avatars0.githubusercontent.com/u/39353470?s=460&u=c82cc7e746e25bdab580cdb83ec41dbb938a7d71&v=4"
               alt="profilepic"
             ></img>
           </div>
-          <div className="pt-4 ml-10 md:text-md sm:text-sm">
-            <div className="pt-5 pb-5">
-              <span class="md:text-2xl sm:text-xl"> {username} </span>
+          <div className="pt-4 ml-10">
+            <div className="md:text-2xl text-md pt-5 pb-2">
+              <span> {username} </span>
             </div>
-            <div className="grid grid-flow-col auto-cols-max gap-20 text-md">
+            <button
+              href="#"
+              className="px-3 py-2 mb-2 rounded-md text-xs bg-white text-black"
+            > EDIT
+            </button>
+            <div className="md:text-lg text-sm gap-5 sm:gap-10 grid grid-flow-col auto-cols-max">
               <div>0 posts</div>
               <div>0 followers</div>
               <div>0 following</div>
             </div>
-            <div className="pb-8 pt-3 md:text-md sm:text-sm">
+            <div className="md:text-lg text-sm pb-8 pt-3">
               <span> {displayName} </span>
               <br />
               <span> {bio} </span>
