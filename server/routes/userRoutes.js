ce31b127dc06ace250aa58f16c8d68c75a7c560e
Callum Taylor <callumtaylorbusiness@gmail.com>
2021-02-04 13:52:35 +0000

    Fixed image not updating.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -164,7 +164,7 @@ module.exports = (app) => {
     var dataURI = req.body.dataURI;
     var uploadStr = '' + dataURI;
     let user = null;
-    let public_id = id + "pp";
+    let public_id = id + "pp" + Date.now();
     cloudinary.uploader.destroy(public_id, {invalidate: true})
     cloudinary.uploader.upload(uploadStr, {public_id: public_id, overwrite: true}, async function (error, result) {
       user = await User.findByIdAndUpdate(id, {profileImage: public_id});
