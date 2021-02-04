e62bb9157ed29f9a661b5de4eed2124721cd54c1
Harris Uddin <harris7001@gmail.com>
2021-02-04 13:48:25 +0000

    fixed issue with profile post css

--- client/src/components/ProfilePostCard.js
+++ client/src/components/ProfilePostCard.js
@@ -1,31 +1,38 @@
 import React from "react";
-import {Image} from 'cloudinary-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons'
+import { Image } from "cloudinary-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faStar } from "@fortawesome/free-solid-svg-icons";
 
+const heart = <FontAwesomeIcon icon={faHeart} />;
+const star = <FontAwesomeIcon icon={faStar} />;
 
-const heart = <FontAwesomeIcon icon={faHeart} />
-const star = <FontAwesomeIcon icon={faStar} />
-
-function ProfilePostCard (props) {
-
+function ProfilePostCard(props) {
   return (
-    <div className='gallery-item square-box'>
-      <div className='square-content'>
-        <Image className="gallery-image" cloudName="bluetooth" publicId={props.image} secure="true"></Image>
+    <div className="gallery-item square-box">
+      <div className="square-content">
+        <Image
+          className="gallery-image"
+          cloudName="bluetooth"
+          publicId={props.image}
+          secure="true"
+        ></Image>
         <div className="gallery-item-info mx-auto">
           <ul>
             <li>
-              <span className="font-extrabold text-sm md:text-xl"> {heart} {props.likes} </span>
+              <span className="font-extrabold text-sm md:text-xl">
+                {heart} {props.likes}
+              </span>
             </li>
             <li>
-              <span className="font-extrabold text-sm md:text-xl"> {star} {props.points} </span>
+              <span className="font-extrabold text-sm md:text-xl">
+                {star} {props.points}
+              </span>
             </li>
           </ul>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default ProfilePostCard;
