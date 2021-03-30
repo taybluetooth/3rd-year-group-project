06d413bbb9fc3fe2a4c21f223ca633877c0b56b2
Uddin, Harris <harris7001@gmail.com>
2021-03-30 01:09:46 +0100

    fix responsiveness of suggested users

--- client/src/pages/SuggestedUsers.js
+++ client/src/pages/SuggestedUsers.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Appbar from "../components/Appbar";
@@ -9,11 +9,10 @@ import userService from "../services/userService";
 import Loading from "../components/Loading";
 
 const SuggestedUsers = () => {
-
   const [users, setUsers] = useState(null);
 
   useEffect(() => {
-    if(!users) {
+    if (!users) {
       getData();
     }
   });
@@ -21,54 +20,57 @@ const SuggestedUsers = () => {
   const getData = async () => {
     let res = await userService.getAll();
     setUsers(res);
-  }
+  };
 
   const renderEntry = (user) => {
     return (
-      <li className="flex justify-between border-b border-gray-200">
+      <li
+        key={user._id}
+        className="flex justify-between border-b border-gray-200"
+      >
         <div className="flex items-center challenges-left md:mr-3 mr-6">
           <div className="challenges-user flex items-center">
             <Image
-              className="gallery-image rounded-full"
+              className="object-cover block"
               cloudName="bluetooth"
               alt="profilepic"
               publicId={user.profileImage}
               secure="true"
             ></Image>
-          <span className="text-gray-500 ml-3">{user.username}</span>
+            <div className="text-gray-500 ml-3 truncate w-24 sm:w-full">
+              {user.username}
+            </div>
           </div>
         </div>
         <div className="flex items-center">
-        <Link className="px-3 py-2 mb-2 rounded-md text-md bg-black text-white" to={`/profile/${user.username}`}>View Profile</Link>
+          <Link
+            className="px-3 py-2 mb-2 rounded-md text-md bg-black text-white"
+            to={`/profile/${user.username}`}
+          >
+            View
+          </Link>
         </div>
       </li>
-    )
+    );
   };
 
   return (
     <div className="h-screen">
       <Appbar />
-      <div className="flex justify-center mx-auto p-4">
-        <div className="md:w-4/12 w-12/12 mx-3 text-white overflow-hidden md:mt-6">
+      <div className="flex justify-center mx-auto sm:p-4">
+        <div className="md:w-3/5 mx-3 text-white overflow-hidden md:mt-6">
           <div className="md:w-full container p-3">
             <ul className="bg-white rounded-md text-black challenges-list">
               <div className="text-2xl text-gray-700 pb-1 font-semibold border-b border-gray-200">
                 Discover Users
               </div>
-              {
-                users ? (
-                  users.map((user) => renderEntry(user))
-                )
-                : (
-                  <Loading />
-                )
-              }
+              {users ? users.map((user) => renderEntry(user)) : <Loading />}
             </ul>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default SuggestedUsers;
